8f490ab23056b100da84109910575a49a5f0a421
ecigwegbu <igwegbu@gmail.com>
2023-02-27 17:21:59 +0100

    0. New node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,31 +9,36 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t temp;
+	binary_tree_t *temp;
 
-	/* perform NULL pointer checks */
-	if (parent == NULL)
-	{
-		/* root node */
-	}
-
-	temp = malloc(sizeof(binary_tree_t))
+	temp = malloc(sizeof(binary_tree_t));
 	if (temp == NULL)
 	{
 		return (NULL);
 	}
 
+	/* set the value of the node */
 	temp->n = value;
-	temp->parent = parent;
-	if (parent != NULL)
-	{
-		if (parent->left != NULL)
-			parent->left = temp;
-		else if (parent->right != NULL)
-			parent->right = temp;
-	}
+
+	/* set child nodes to NULL */
 	temp->left = NULL;
 	temp->right = NULL;
 
+	/* try to set parent if there is space for another  child */
+	if (parent != NULL)
+	{
+		if (parent->left == NULL)
+			parent->left = temp;
+		else if (parent->right == NULL)
+			parent->right = temp;
+		else
+		{
+			printf("Can't add a third child to binary tree node.\n");
+			return (NULL);
+		}
+	}
+
+	/* now you are an adopted  child :), who is your parent? */
+	temp->parent = parent;
 	return (temp);
 }
