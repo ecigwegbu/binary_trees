f5021d6151663f2b1d488e1dad1601057ee4833c
ecigwegbu <igwegbu@gmail.com>
2023-02-28 20:45:31 +0100

    16. Is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -18,7 +18,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	else if (tree->left == NULL && tree->right == NULL)
+	else if ((tree->left == NULL) && (tree->right == NULL))
 		return (1);
 
 	/* get tree height */
