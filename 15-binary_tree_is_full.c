2835bbc1be9624ff3af41e2db4f6da346edc82a0
ecigwegbu <igwegbu@gmail.com>
2023-02-28 23:03:54 +0100

    15. Is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,23 +1,45 @@
 #include "binary_trees.h"
 
+void bt_is_full(const binary_tree_t *tree, int *isFull);
+
 /**
- * binary_tree_is_full - check if a binary tree is a full binary tree
- * @tree: the root node of the tree to check
- * Return: 1 if it is a full binary tree, 0 otherwise
+ * binary_tree_is_full - check if a binary tree is full
+ * @tree: pointer to the root node of the binary tree
+ * Return: 1 if full 0 otherwise
  */
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	int isFull = 1;
+
 	/* NULL check */
 	if (tree == NULL)
 		return (1);
-
-	/* check if children are empty */
-	if (tree->left == NULL && tree->right == NULL)
+	else if (!tree->left && !tree->right)
 		return (1);
 
-	/* recursively call the binary_tree_os_full check on both children */
-	if ((tree->left != NULL) && (tree->right != NULL))
-		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
-	return (0);
+	bt_is_full(tree, &isFull);
+	return (isFull);
+}
+
+/**
+ * bt_is_full - check if a binary true is full
+ * @tree: pointer to the root node of the binary tree
+ * @isFull: pointer to indicator whether tree is full or not
+ */
+
+void bt_is_full(const binary_tree_t *tree, int *isFull)
+{
+	/* recursion terminal condition, not just regular NULL check*/
+	if (tree == NULL)
+		return;
+
+	if (tree && ((tree->left && !tree->right) ||
+			(tree->right && !tree->left)))
+	{
+		*isFull = 0;
+		return;
+	}
+	bt_is_full(tree->left, isFull);
+	bt_is_full(tree->right, isFull);
 }
